0b2cfc1dff49121f4c56285ddc113b43dd1a0e1a
nyan-tama <shake.flake.love@gmail.com>
2024-02-25 18:35:26 +0900

    プロンプト修正

--- chapter-3/app.py
+++ chapter-3/app.py
@@ -2,9 +2,9 @@ import discord
 from discord.ext import commands
 import logging
 import os
-from io import BytesIO # メモリ上で画像のバイナリデータを扱うためのBytesIOをインポートします。
-import base64 # Base64エンコーディング・デコーディングに使用するライブラリをインポートします。同じくイメージ描画の際に使用。
-from generate_ai import bedrock
+from io import BytesIO # メモリ上で画像のバイナリデータを扱うためのBytesIOをインポートします。動作確認用のコードで使用
+import base64 # Base64エンコーディング・デコーディングに使用するライブラリをインポートします。同じくイメージ描画の際に使用
+from generate_ai import generate_monster_bedrock # generate_ai.pyに作った『generate_monster_bedrock』を呼び出す
 
 
 TOKEN = os.getenv('DISCORD_BOT_TOKEN')
@@ -26,22 +26,24 @@ async def make(ctx, *, text: str):
 
     await ctx.send("ただいま作成中...")
 
-    # ユーザーのリクエストに基づいてモンスターの情報（テキストと画像）をBedrockで生成します
-    monster_info = bedrock(text)
+    # Discordから受け取った『text』のメッセージを『generate_monster_bedrock』関数に渡して、文章と画像を生成
+    monster_info = generate_monster_bedrock(text)
 
-    # 第3回では一旦、生成したモンスターの情報をそのままDiscord上で表示します
+    # 第3回では動作確認のため、生成したモンスターの情報をそのままDiscord上で表示してみます
+    # 以降のプログラムは第3回の動作確認用のプログラムなので、第4回以降は削除されます。
+    # -------------------------------------------------------------------------
     monster_details = (
         f"**モンスター名**: {monster_info['monster_name']}\n"
-        f"**レベル**: {monster_info['monster_level']}\n"
+        f"**強さ**: {monster_info['monster_level']}\n"
         f"**属性**: {monster_info['monster_element']}\n"
         f"**特殊能力**: {monster_info['monster_ability']}\n"
         f"**伝説**: {monster_info['monster_episode']}"
     )
 
-    # テキスト結果を送信
+    # テキスト結果をDiscordに送信
     await ctx.send(monster_details)
 
-    # イメージ結果を送信
+    # イメージ結果をDiscordに送信
     image_data_base64 = monster_info['monster_image']
     image_data = base64.b64decode(image_data_base64)
     # BytesIOオブジェクトを使って、メモリ上に画像を保持します
@@ -49,5 +51,6 @@ async def make(ctx, *, text: str):
         # discord.Fileオブジェクトを作成し、画像ファイルとして送信します
         discord_file = discord.File(image_file, filename="monster_image.png")
         await ctx.send(file=discord_file)
-
+    # -------------------------------------------------------------------------
+        
 bot.run(TOKEN)
--- chapter-3/generate_ai.py
+++ chapter-3/generate_ai.py
@@ -1,7 +1,6 @@
 import boto3
 import json
-import logging
-
+import re
 
 # boto3クライアントを初期化
 bedrock_runtime = boto3.client(service_name='bedrock-runtime', region_name='us-east-1')
@@ -12,8 +11,8 @@ def invoke_text_model(prompt):
         'prompt': prompt,
         'max_tokens_to_sample': 200,
         'temperature': 0.9,
-        'top_k': 150,
-        'top_p': 0.7
+        'top_k': 180,
+        'top_p': 0.9
     })
     response = bedrock_runtime.invoke_model(
         modelId='anthropic.claude-v2',
@@ -22,14 +21,20 @@ def invoke_text_model(prompt):
         contentType='application/json'
     )
     response_body = json.loads(response['body'].read())
-    return response_body['completion'].strip(" <answer></answer>")
+
+    # <answer></answer>タグで囲まれたテキストを抽出
+    match = re.search(r'<answer>(.*?)</answer>', response_body['completion'], re.DOTALL)
+    if match:
+        return match.group(1).strip()  # タグ内のテキストのみを返す
+    else:
+        return ""  # マッチしない場合は空文字を返す
 
 def invoke_image_model(prompt):
     body = json.dumps({
         'text_prompts': [{'text': prompt}],
-        'cfg_scale': 5,
-        'seed': 30,
-        'steps': 120
+        'cfg_scale': 9,
+        'seed': 40,
+        'steps': 150
     })
 
     response = bedrock_runtime.invoke_model(
@@ -51,72 +56,76 @@ def translate_text(text, source_language_code, target_language_code):
 
 def generate_prompt_for_name(role_setting, user_request):
     return (
-        f"Human: {role_setting}として、{user_request}に名前を付けてください。"
-        "人間らしいファーストネームとミドルネームを持つ、このモンスターの名前を考えて、<answer></answer>タグ内に記入してください。\n"
+        f"Human: {role_setting}として、ユーザーが{user_request}をリクエストしています。"
+        "人間らしいファーストネームとミドルネームを持つ、このモンスターの名前を考えて、<answer></answer>タグ内に記入してください。"
+        "ユーザーが指定した名前があるならばそれを最優先で反映してください。\n"
         "Assistant: "
     )
 
 def generate_prompt_for_level(role_setting, user_request):
     return (
-        f"Human: {role_setting}として、{user_request}の強さのレベルを設定してください。"
-        "1から10までのスケールで、適切なレベルを選び、その数値を<answer></answer>タグ内に記入してください。レベルが高いほど、モンスターは強力です。\n"
+        f"Human: {role_setting}として、ユーザーが{user_request}をリクエストしています。"
+        "1から10までの強さがあり、その数値を<answer></answer>タグ内に記入してください。強さが大きいほど、モンスターは強力です。"
+        "ユーザーが指定した強さがあるならばそれを最優先で反映してください。\n"
         "Assistant: "
     )
 
 def generate_prompt_for_element(role_setting, user_request):
     return (
-        f"Human: {role_setting}として、{user_request}にどの属性を与えるか決定してください。"
-        "火、水、風、土、光、闇の中から、最も適した属性を選び、その属性を<answer></answer>タグ内に記入してください。\n"
+        f"Human: {role_setting}として、ユーザーが{user_request}をリクエストしています。"
+        "火、水、風、土、光、闇の中から、最も適した属性を選び、その属性を<answer></answer>タグ内に記入してください。"
+        "ユーザーが指定した属性があるならばそれを最優先で反映してください。\n"
         "Assistant: "
     )
 
 def generate_prompt_for_ability(role_setting, user_request):
     return (
-        f"Human: {role_setting}として、{user_request}に基づく特殊能力を100文字以内で考えてください。"
-        f"モンスターの特殊能力とその説明を<answer>【特殊能力】：説明</answer>タグ内に簡潔に記述してください。\n"
+        f"Human: {role_setting}として、ユーザーが{user_request}をリクエストしています。"
+        "モンスターの特殊能力とその説明を<answer>【特殊能力】：説明</answer>タグ内に100文字程度で記述してください。"
+        "ユーザーが指定した特殊能力があるならばそれを最優先で反映してください。\n"
         "Assistant: "
     )
 
 def generate_prompt_for_episode(role_setting, user_request, prompt_level, monster_element, prompt_ability):
     return (
-        f"Human: {role_setting}として、{user_request}のバックグラウンドがわかる悲しく恐ろしい言い伝えを100文字以内で考えてください。"
+        f"Human: {role_setting}として、ユーザーが{user_request}をリクエストしています。"
         f"モンスターの属性である{monster_element}、数値が大きいほど強い1~10段階ある強さの中でレベル{prompt_level}、特殊能力が{prompt_ability}であることを考慮してください。"
-        f"作成したエピソードを<answer></answer>タグ内に記入してください。\n"
-        f"Assistant: "
+        "このモンスターの不気味で悲しく謎に満ちた伝説を<answer></answer>タグ内に100文字程度で記述してください。"
+        "ユーザーが指定した伝説があるならばそれを最優先で反映してください。\n"
+        "Assistant: "
     )
 
-def generate_prompt_for_image(role_setting, user_request, monster_ability, monster_episode):
+def generate_prompt_for_image(role_setting, user_request, monster_episode):
     return (
         f"{role_setting}として、{user_request}の画像を生成してください。"
-        f"トレーディングカードにふさわしい鮮やかな色彩と、詳細なテクスチャを持つファンタジースタイルを希望します。"
-        f"モンスターはカードの中心に配置され、背景はモンスターの物語である{monster_episode}を象徴する要素で満たされています。"
-        f"モンスターのポーズは特殊能力である{monster_ability}を参考に描いてください。"
+        "彩度が強く鮮やかな色彩と、緻密で詳細なテクスチャを持つファンタジースタイルを希望します。"
+        f"モンスターはカードの中心に配置され、背景はモンスターの物語である{monster_episode}の物語を表現する要素で満たされています。"
     )
 
-def bedrock(user_request):
-    role_setting = "あなたはファンタジーに詳しい知識人で画家"
+def generate_monster_bedrock(user_request):
+    # 役割の設定
+    role_setting = "あなたはファンタジーに詳しいクリエイターです"
 
-    # 各特性のプロンプト生成
+    # 各特性のプロンプト成形
     prompt_name = generate_prompt_for_name(role_setting, user_request)
-    monster_name = invoke_text_model(prompt_name)
-    
     prompt_level = generate_prompt_for_level(role_setting, user_request)
-    monster_level = invoke_text_model(prompt_level)
-    
     prompt_element = generate_prompt_for_element(role_setting, user_request)
-    monster_element = invoke_text_model(prompt_element)
-    
     prompt_ability = generate_prompt_for_ability(role_setting, user_request)
-    monster_ability = invoke_text_model(prompt_ability)
-    
     prompt_episode = generate_prompt_for_episode(role_setting, user_request, monster_level, monster_element, monster_ability)
+
+    prompt_image = generate_prompt_for_image(role_setting, user_request, monster_episode)
+    translated_prompt = translate_text(prompt_image, 'ja', 'en')
+
+    # 成形したプロンプトでbedrockから文章と画像の生成
+    monster_name = invoke_text_model(prompt_name)
+    monster_level = invoke_text_model(prompt_level)
+    monster_element = invoke_text_model(prompt_element)
+    monster_ability = invoke_text_model(prompt_ability)
     monster_episode = invoke_text_model(prompt_episode)
 
-    # モンスター画像生成
-    image_prompt = generate_prompt_for_image(role_setting, user_request, monster_ability, monster_episode)
-    translated_prompt = translate_text(image_prompt, 'ja', 'en')
     monster_image = invoke_image_model(translated_prompt)
 
+    # 結果のレスポンス
     return {
         "monster_name": monster_name,
         "monster_level": monster_level,
