0d4b319f714d2258b4e063402ffa987418f2db61
nyan-tama <shake.flake.love@gmail.com>
2024-03-06 20:16:44 +0900

    コード完成

--- chapter-5/app.py
+++ chapter-5/app.py
@@ -3,11 +3,28 @@ from discord.ext import commands
 import logging
 import os
 from generate_ai import generate_monster_bedrock
-# image_processing.pyに作った『generate_card_and_upload_image』関数を呼び出す
-from image_processing import generate_card_and_upload_image
+from image_processing import generate_card
+import boto3 # AWSサービス連携用
 
+# AWS Secrets Managerと連携
+secrets_manager_client = boto3.client('secretsmanager')
+# AWS S3と連携
+s3_client = boto3.client('s3')
 
-TOKEN = os.getenv('DISCORD_BOT_TOKEN')
+# AWS Secrets Managerからシークレット情報を取得する関数
+def get_secret(secret_name):
+    try:
+        # AWS Secrets Managerから指定したシークレットの値を取得します。
+        get_secret_value_response = secrets_manager_client.get_secret_value(SecretId=secret_name)
+        return get_secret_value_response['SecretString']
+    except Exception as e:
+        logging.error(f"シークレットの取得中にエラーが発生しました: {e}")
+        return None
+
+# シークレット名を指定してDiscord BotのTOKENを取得
+TOKEN = get_secret('DISCORD_BOT_TOKEN') 
+# シークレット名を指定しS3のBUCKET名を取得
+BUCKET_NAME = get_secret('MY_S3_BUCKET_NAME')
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s:%(levelname)s: %(message)s')
 
@@ -28,7 +45,25 @@ async def make(ctx, *, text: str):
 
     monster_info = generate_monster_bedrock(text)
 
-    # 生成したモンスター情報を使用してカードをレイアウトし、画像に変換
-    generate_card_and_upload_image(monster_info)
+    image_path = generate_card(monster_info)
+
+    # 生成された画像をDiscordに送信
+    with open(image_path, 'rb') as file:
+        # Discordに画像を送信
+        await ctx.send(file=discord.File(file, filename=image_path))
+
+        # ファイルの読み込み位置を先頭に戻す
+        file.seek(0)
+
+        # 画像をS3にアップロード
+        s3_client.upload_fileobj(file, BUCKET_NAME, os.path.basename(image_path))
+
+        logging.info(f'{image_path} を {BUCKET_NAME} にアップロードしました。')
+
+    # ローカルのファイルを削除
+    os.remove(image_path)
+    logging.info(f'ローカル上のファイル {image_path} は削除しました。')
     
+    logging.info(f'すべての処理が完了しました。')
+
 bot.run(TOKEN)
